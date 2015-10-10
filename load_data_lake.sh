ba48c54af1609d83faa2c5dfefa37e4c7b89bf31
Johnny Yeo <johnnyyeo@Johnnys-MacBook-Pro.local>
2015-10-10 12:08:52 -0700

    added code re:putting data into hdfs

--- load_data_lake.sh
+++ load_data_lake.sh
@@ -1,7 +1,9 @@
-cd ~/Desktop/exercise1/hospital_compare/
+cd /root/exercise1/hospital_compare/
 
 tail -n +2 "Hospital General Information.csv" > hospitals.csv
 tail -n +2 "Timely and Effective Care - Hospital.csv" > effective_care.csv
 tail -n +2 "Readmissions and Deaths - Hospital.csv" > readmissions.csv
 tail -n +2 hvbp_hcahps_05_28_2015.csv > surveys_responses.csv
 mv "Measure Dates.csv" measure_dates.csv
+
+$HADOOP_HOME/bin/hadoop fs -put /root/exercise1/hospital_compare .
