dc024766a4b15bb6e92f491edce34e971873c389
Johnny Yeo <johnnyyeo@Johnnys-MacBook-Pro.local>
2015-10-10 11:24:18 -0700

    added load_data_lake.sh unix script for renaming files and removing title rows

---
+++ load_data_lake.sh
@@ -0,0 +1,7 @@
+cd ~/Desktop/exercise1/hospital_compare/
+
+tail -n +2 "Hospital General Information.csv" > hospitals.csv
+tail -n +2 "Timely and Effective Care - Hospital.csv" > effective_care.csv
+tail -n +2 "Readmissions and Deaths - Hospital.csv" > readmissions.csv
+tail -n +2 hvbp_hcahps_05_28_2015.csv > surveys_responses.csv
+mv "Measure Dates.csv" measure_dates.csv
