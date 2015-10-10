a2d5a4c5c411b73d2fb2fff82e68e8a3575bd17f
Johnny Yeo <johnnyyeo@Johnnys-MacBook-Pro.local>
2015-10-10 12:11:40 -0700

    bug fix to last line of code

--- load_data_lake.sh
+++ load_data_lake.sh
@@ -6,4 +6,4 @@ tail -n +2 "Readmissions and Deaths - Hospital.csv" > readmissions.csv
 tail -n +2 hvbp_hcahps_05_28_2015.csv > surveys_responses.csv
 mv "Measure Dates.csv" measure_dates.csv
 
-$HADOOP_HOME/bin/hadoop fs -put /root/exercise1/hospital_compare .
+$HADOOP_HOME/bin/hadoop fs -put /root/exercise1/hospital_compare $HADOOP_HOME/bin
