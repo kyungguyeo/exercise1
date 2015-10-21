0019d078779633fdd73ac89af3a163ce64f9637b
Johnny Yeo <johnnyyeo@Johnnys-MacBook-Pro.local>
2015-10-20 22:43:24 -0700

    cleaning out master

--- investigations/hospital_variability/variance.sql
+++
@@ -1,1 +0,0 @@
-SELECT DISTINCT a.measure_id, ROUND(a.sd_score, 2) as sd_score, a.range_score FROM (SELECT e.measure_id, STDDEV_POP(e.score) as sd_score, MAX(e.score) - MIN(e.score) as range_score FROM effective_care_tr e GROUP BY e.measure_id) a ORDER BY sd_score desc limit 10;
--- investigations/hospitals_and_patients/corr.sql
+++
@@ -1,5 +0,0 @@
---Correlation Between Hospital Quality Scores (base_score and consistency_score) and Survey Scores
-SELECT ROUND(CORR(CAST(a.survey_score AS DOUBLE), CAST(a.quality_score AS DOUBLE)),2) as corr FROM (SELECT DISTINCT h.provider_id, h.hospital_name, h.survey_score, (h.base_score + h.consistency_score)/2 quality_score FROM hospital_summary_scores h) a;
-
---Correlation Between Variance in Procedure and Survey Scores
-SELECT ROUND(CORR(CAST(h.survey_score AS DOUBLE), CAST(a.sd AS DOUBLE)),2) as corr FROM (SELECT provider_id, STDDEV_POP(score) as sd FROM effective_care_tr GROUP BY provider_id) a INNER JOIN hospital_summary_scores h ON (a.provider_id = h.provider_id);
